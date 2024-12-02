e42368dc6dade88edc0c0e43bdb8eab4930f5253
Daniel-gk <126163283+Daniel-gk@users.noreply.github.com>
2024-12-02 08:52:06 -0300

    asdcfvbnm,.

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,5 +1,5 @@
-const url = 'https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/trabalho/trabalho-dados-gerais.json'
-async function quantidadeUsuarios() {    
+async function quantidadeUsuarios() {
+const url = 'https://github.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/blob/Aula01/trabalho/trabalho-dados-gerais.json'    
     const res = await fetch(url)
     const dados = await res.json()
     const pessoas_mundo = Object.keys(dados)
